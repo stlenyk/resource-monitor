14ba8b356308e8308068f4427629492a3649c465
stlenyk <stlenyk@gmail.com>
2023-08-05 17:20:38 +0200

    Fix main panel stuttering

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,9 @@
+use crate::send_types::*;
+
 use std::{collections::VecDeque, time::Duration};
 
 use leptos::*;
-use plotly::{
-    color::{Rgb, Rgba},
-    common::{Line, Marker},
-    layout::{self, Axis, Margin},
-    plot, Configuration, Layout, Plot,
-};
+use plotly::{color::Rgb, common::Marker, layout::Margin, Plot};
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
@@ -21,10 +18,11 @@ struct GreetArgs<'a> {
     name: &'a str,
 }
 
-fn plot_cpu(sys_util_history: &VecDeque<SystemUtilization>, max_history: usize) -> plotly::Plot {
+// Assumes that the number of cpus doesn't change and may panic otherwise.
+fn plot_cpu(sys_util_history: &VecDeque<SystemUtilization>, max_history: usize) -> Plot {
     let mut plot = Plot::new();
 
-    let config = plotly::configuration::Configuration::new().static_plot(true);
+    let config = plotly::Configuration::new().static_plot(true);
     plot.set_configuration(config);
 
     let cpu_history = sys_util_history
@@ -49,6 +47,8 @@ fn plot_cpu(sys_util_history: &VecDeque<SystemUtilization>, max_history: usize) 
             Rgb::new(120, 149, 203),
             Rgb::new(160, 191, 224),
             Rgb::new(197, 223, 248),
+            Rgb::new(160, 191, 224),
+            Rgb::new(120, 149, 203),
         ];
         for (i, y) in traces.iter().enumerate() {
             let color = colors[i % colors.len()];
@@ -64,7 +64,7 @@ fn plot_cpu(sys_util_history: &VecDeque<SystemUtilization>, max_history: usize) 
 
 fn plot_mem(sys_util_history: &VecDeque<SystemUtilization>, max_history: usize) -> plotly::Plot {
     let mut plot = Plot::new();
-    let config = plotly::configuration::Configuration::new().static_plot(true);
+    let config = plotly::Configuration::new().static_plot(true);
     plot.set_configuration(config);
 
     let mem_history = sys_util_history
@@ -84,108 +84,6 @@ fn plot_mem(sys_util_history: &VecDeque<SystemUtilization>, max_history: usize) 
 
     plot
 }
-
-/// NB: assumes that the number of cpus doesn't change. May panic otherwise.
-#[component]
-fn PlotCpu(
-    cx: Scope,
-    sys_util_history: ReadSignal<VecDeque<SystemUtilization>>,
-    max_history: usize,
-    max_history_time: Duration,
-) -> impl IntoView {
-    let div_id = "main-cpu";
-
-    create_effect(cx, move |_| {
-        let mut plot = plot_cpu(&sys_util_history.get(), max_history);
-
-        let title =
-            plotly::common::Title::new(&format!("history len: {}", sys_util_history.get().len()));
-
-        let black = plotly::color::Rgb::new(0, 0, 0);
-        let y_ticks = vec![0.0, 20.0, 40.0, 60.0, 80.0, 100.0];
-        let y_ticks_text = y_ticks.iter().map(|x| format!("{:.0}%", x)).collect();
-        let y_axis = plotly::layout::Axis::new()
-            .range(vec![0, 100])
-            .tick_values(y_ticks)
-            .tick_text(y_ticks_text)
-            .side(plotly::common::AxisSide::Right)
-            .line_color(black)
-            .mirror(true);
-        let x_axis = plotly::layout::Axis::new()
-            .range(vec![0, max_history - 1])
-            .tick_values(vec![0.0])
-            .tick_text(vec![format!("{} s", max_history_time.as_secs())])
-            .line_color(black)
-            .mirror(true);
-        let _transparent = plotly::color::Rgba::new(0, 0, 0, 0.0);
-        let layout = plotly::layout::Layout::new()
-            // .paper_background_color(transparent)
-            .auto_size(true)
-            .title(title)
-            .y_axis(y_axis)
-            .x_axis(x_axis);
-        plot.set_layout(layout);
-
-        spawn_local(async move {
-            plotly::bindings::react(div_id, &plot).await;
-        });
-    });
-
-    view! {cx,
-        <div id={div_id}></div>
-    }
-}
-
-#[component]
-fn PlotMem(
-    cx: Scope,
-    sys_util_history: ReadSignal<VecDeque<SystemUtilization>>,
-    max_history: usize,
-    max_history_time: Duration,
-) -> impl IntoView {
-    let div_id = "main-mem";
-
-    create_effect(cx, move |_| {
-        let mut plot = plot_mem(&sys_util_history.get(), max_history);
-
-        let title =
-            plotly::common::Title::new(&format!("history len: {}", sys_util_history.get().len()));
-
-        let black = plotly::color::Rgb::new(0, 0, 0);
-        let max_mem = if let Some(sys_util) = sys_util_history.get().front() {
-            sys_util.mem_max
-        } else {
-            0
-        };
-        let y_axis = plotly::layout::Axis::new()
-            .range(vec![0, max_mem])
-            .side(plotly::common::AxisSide::Right)
-            .line_color(black)
-            .mirror(true);
-        let x_axis = plotly::layout::Axis::new()
-            .range(vec![0, max_history - 1])
-            .tick_values(vec![0.0])
-            .tick_text(vec![format!("{} s", max_history_time.as_secs())])
-            .line_color(black)
-            .mirror(true);
-        let layout = plotly::layout::Layout::new()
-            .auto_size(true)
-            .title(title)
-            .y_axis(y_axis)
-            .x_axis(x_axis);
-        plot.set_layout(layout);
-
-        spawn_local(async move {
-            plotly::bindings::react(div_id, &plot).await;
-        });
-    });
-
-    view! {cx,
-        <div id={div_id}></div>
-    }
-}
-
-use crate::send_types::*;
 
 #[component]
 fn PlotCpuMini(
@@ -268,7 +166,6 @@ fn SidePanel(
     sys_util_history: ReadSignal<VecDeque<SystemUtilization>>,
     max_history: usize,
 ) -> impl IntoView {
-
     view! {cx,
         <div class="left">
             <button on:click=move |_| {main_view.set(MainView::Cpu)}>
@@ -284,6 +181,19 @@ fn SidePanel(
     }
 }
 
+#[allow(unused)]
+fn print_bytes(value: UtilMem) -> String {
+    let mut value = value as f32;
+    let suffixes = ["KB", "MB", "GB", "TB"];
+    let base = 1024.0;
+    let mut pow = 0;
+    while value > base {
+        value /= base;
+        pow += 1;
+    }
+    format!("{:.1} {}", value, suffixes.get(pow).unwrap_or(&"TB"))
+}
+
 #[component]
 fn MainPanel(
     cx: Scope,
@@ -292,18 +202,88 @@ fn MainPanel(
     max_history: usize,
     max_history_time: Duration,
 ) -> impl IntoView {
-    let plot = move || match main_view.get() {
-        MainView::Cpu => {
-            view! {cx, <PlotCpu sys_util_history=sys_util_history max_history=max_history max_history_time=max_history_time/>}
+    let div_id = "main-view";
+
+    create_effect(cx, move |_| {
+        match main_view.get() {
+            MainView::Cpu => {
+                let mut plot = plot_cpu(&sys_util_history.get(), max_history);
+
+                let title = plotly::common::Title::new(&format!(
+                    "history len: {}",
+                    sys_util_history.get().len()
+                ));
+
+                let black = plotly::color::Rgb::new(0, 0, 0);
+                let y_ticks = vec![0.0, 20.0, 40.0, 60.0, 80.0, 100.0];
+                let y_ticks_text = y_ticks.iter().map(|x| format!("{:.0}%", x)).collect();
+                let y_axis = plotly::layout::Axis::new()
+                    .range(vec![0, 100])
+                    .tick_values(y_ticks)
+                    .tick_text(y_ticks_text)
+                    .side(plotly::common::AxisSide::Right)
+                    .line_color(black)
+                    .mirror(true);
+                let x_axis = plotly::layout::Axis::new()
+                    .range(vec![0, max_history - 1])
+                    .tick_values(vec![0.0])
+                    .tick_text(vec![format!("{} s", max_history_time.as_secs())])
+                    .line_color(black)
+                    .mirror(true);
+                let _transparent = plotly::color::Rgba::new(0, 0, 0, 0.0);
+                let layout = plotly::layout::Layout::new()
+                    // .paper_background_color(transparent)
+                    .auto_size(true)
+                    .title(title)
+                    .y_axis(y_axis)
+                    .x_axis(x_axis);
+                plot.set_layout(layout);
+
+                spawn_local(async move {
+                    plotly::bindings::react(div_id, &plot).await;
+                });
+            }
+
+            MainView::Mem => {
+                let mut plot = plot_mem(&sys_util_history.get(), max_history);
+
+                let title = plotly::common::Title::new(&format!(
+                    "history len: {}",
+                    sys_util_history.get().len()
+                ));
+
+                let black = plotly::color::Rgb::new(0, 0, 0);
+                let mem_max = sys_util_history
+                    .get()
+                    .get(0)
+                    .map_or(0, |sys_util| sys_util.mem_max);
+                let y_axis = plotly::layout::Axis::new()
+                    .range(vec![0, mem_max])
+                    .side(plotly::common::AxisSide::Right)
+                    .line_color(black)
+                    .mirror(true);
+                let x_axis = plotly::layout::Axis::new()
+                    .range(vec![0, max_history - 1])
+                    .tick_values(vec![0.0])
+                    .tick_text(vec![format!("{} s", max_history_time.as_secs())])
+                    .line_color(black)
+                    .mirror(true);
+                let layout = plotly::layout::Layout::new()
+                    .auto_size(true)
+                    .title(title)
+                    .y_axis(y_axis)
+                    .x_axis(x_axis);
+                plot.set_layout(layout);
+
+                spawn_local(async move {
+                    plotly::bindings::react(div_id, &plot).await;
+                });
+            }
         }
-        MainView::Mem => {
-            view! {cx, <PlotMem sys_util_history=sys_util_history max_history=max_history max_history_time=max_history_time/>}
-        }
-    };
+    });
+
     view! {cx,
-        <div class="right">
-            {plot}
-        </div>
+        <div class="right" id=div_id></div>
     }
 }
 
@@ -320,7 +300,7 @@ fn SysInfo(cx: Scope) -> impl IntoView {
     let max_history = (max_history_time.as_millis() / update_interval.as_millis()) as usize;
 
     let (sys_util_history, set_sys_util) = create_signal(cx, VecDeque::with_capacity(max_history));
-    let (main_view, set_main_view) = create_signal(cx, MainView::Cpu);
+    let (main_view, set_main_view) = create_signal(cx, MainView::Mem);
 
     let update_sys_util = move || {
         spawn_local(async move {
@@ -334,6 +314,7 @@ fn SysInfo(cx: Scope) -> impl IntoView {
             set_sys_util.set(history);
         });
     };
+    update_sys_util();
 
     set_interval(update_sys_util, update_interval);
 
@@ -346,7 +327,7 @@ fn SysInfo(cx: Scope) -> impl IntoView {
                 }
             }</p>
             <SidePanel main_view=set_main_view sys_util_history=sys_util_history max_history=max_history/>
-            // <MainPanel main_view=main_view sys_util_history=sys_util_history max_history=max_history max_history_time=max_history_time/>
+            <MainPanel main_view=main_view sys_util_history=sys_util_history max_history=max_history max_history_time=max_history_time/>
         </div>
     }
 }
