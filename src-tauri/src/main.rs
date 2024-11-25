129ad7e84c71791b32a06f86968635d67cb275ea
stlenyk <stlenyk@gmail.com>
2024-11-25 01:49:56 +0100

    Bump dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -106,7 +106,7 @@ impl SystemMonitor {
                 .with_cpu_usage()
                 .with_frequency(),
         );
-        self.sys.refresh_processes(sysinfo::ProcessesToUpdate::All);
+        self.sys.refresh_processes(sysinfo::ProcessesToUpdate::All, true);
         self.sys.refresh_memory();
 
         let cpus = self
